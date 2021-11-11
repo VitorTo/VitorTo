9b68312ac895829c6da5b56cb218d14504cf1bae
Vitor Gabriel <72284310+VitorTo@users.noreply.github.com>
2021-11-11 15:46:16 -0300

    Create index.js

---
+++ themes/index.js
@@ -0,0 +1,11 @@
+const themes = { 
+green_gradient: {
+    title_color: "00341B",
+    icon_color: "0CF574",
+    text_color: "2F97C1",
+    bg_color: "000000","00341B","00C064";
+
+},
+};
+
+module.exports = themes;
